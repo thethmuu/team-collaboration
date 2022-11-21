5ab98f4dfe2ce035c4b274772526363ba48d02a8
Solo Hsu <53265779+SoloHsu-coder@users.noreply.github.com>
2022-11-21 19:33:07 +0630

    navbar structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex justify-between p-10">
+      <h2 className="text-xl">W</h2>
+      <ul className="flex justify-between gap-36">
+        <li>Home</li>
+        <li>New</li>
+        <li>Popular</li>
+        <li>Trending</li>
+        <li>Categories</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
